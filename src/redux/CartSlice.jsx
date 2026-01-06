e88871cd2de73fe50327abb36446c8adf335d047
SherleyChintala7 <sherleychintala77@gmail.com>
2026-01-06 16:56:13 +0530

    fourth commit

--- src/redux/CartSlice.jsx
+++ src/redux/CartSlice.jsx
@@ -5,43 +5,48 @@ let cartData = JSON.parse(localStorage.getItem("cartData")) || [];
 let CartSlice = createSlice({
     name: "cart",
     initialState: cartData,
+
     reducers: {
         ADDTOCART: (state, action) => {
-            let product = action.payload;
-            let existingItem = state.find(item => item.id === product.id);
+            let product = action.payload
+            let existingItem = state.find(item => item.id == product.id)
 
             if (existingItem) {
-                existingItem.quantity += 1;
-            } else {
-                state.push(product);
+                existingItem.quantity += 1
             }
-
-            localStorage.setItem("cartData", JSON.stringify(state));
+            else {
+                state.push(product)
+            }
+            localStorage.setItem("cartData", JSON.stringify(state))
         },
 
         REMOVE: (state, action) => {
-            let product = action.payload;
-            let updatedcart = state.filter(item => item.id !== product.id);
-            localStorage.setItem("cartData", JSON.stringify(updatedcart));
-            return updatedcart;
+            let product = action.payload
+            let updatedcart = state.filter(item => item.id !== product.id)
+            localStorage.setItem("cartData", JSON.stringify(updatedcart))
+            return updatedcart
         },
 
         Increment: (state, action) => {
-            const product = state.find(item => item.id === action.payload);
+            const product = state.find(
+                item => item.id === action.payload
+            );
+
             product && (product.quantity += 1);
         },
 
         Decrement: (state, action) => {
-            const product = state.find(item => item.id === action.payload);
+            const product = state.find(
+                item => item.id === action.payload
+            );
 
             if (product.quantity > 1) {
-                product.quantity -= 1;
+                product.quantity -= 1
             } else {
                 return state.filter(item => item.id !== action.payload);
             }
         }
-    }
-});
-
-export const { ADDTOCART, REMOVE, Increment, Decrement } = CartSlice.actions;
-export default CartSlice.reducer;
+    }})
+export const { ADDTOCART,REMOVE } = CartSlice.actions
+export const {Increment,Decrement}=CartSlice.actions
+export default CartSlice.reducer
